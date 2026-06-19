b1e0a6b0044c2f6af1d08b2ea5f1541b4490f29a
Daniel <114138438+Fishkofff@users.noreply.github.com>
2026-06-20 01:59:30 +0300

    задание 1.3

---
+++ HW1/Task_1_3.java
@@ -0,0 +1,8 @@
+public class Task_1_3 {
+    public static void main(String[] args) {
+        System.out.println("Имя: Даниил");
+        System.out.println("Результат выражения '123 * 456' = " + 123 * 456);
+        System.out.println("10 звёздочек: " + "*".repeat(10));
+        System.out.print("Консольное" + " приложение");
+    }
+}
