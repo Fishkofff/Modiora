9f2b90afdc5ea52ddb5452def231b74bf41c3569
Daniel <114138438+Fishkofff@users.noreply.github.com>
2026-06-19 23:10:38 +0300

    hw1: задание 1.2

---
+++ HW1/Task_1_2.java
@@ -0,0 +1,7 @@
+public class Task_1_2 {
+    public static void main(String[] args) {
+        System.out.println("Имя: Даниил");
+        System.out.println("Возраст: 19 лет");
+        System.out.println("Любимый язык программирования: Java");
+    }
+}
