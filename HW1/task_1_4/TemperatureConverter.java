406a8b842b450159e1db1745be0610ea0be1b7dc
Daniel <114138438+Fishkofff@users.noreply.github.com>
2026-06-21 23:59:59 +0300

    задание 1_4.2 (написание класса)

--- HW1/task_1_4/TemperatureConverter.java
+++ HW1/task_1_4/TemperatureConverter.java
@@ -1,4 +1,24 @@
 package task_1_4;
 
 public class TemperatureConverter {
+
+    public static final double ABSOLUTE_ZERO_CELSIUS = -273.15;
+    public static final double BOILING_POINT_CELSIUS = 100.0;
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 1.8) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) / 1.8;
+    }
+
+    public static void main(String[] args) {
+        double currentTemperature = 101;
+        boolean isAboveBoiling = currentTemperature > BOILING_POINT_CELSIUS;
+
+        System.out.println("Температура: " + currentTemperature + " C");
+        System.out.println("Выше точки кипения? " + isAboveBoiling);
+        System.out.println("В Фаренгейтах: " + celsiusToFahrenheit(currentTemperature));
+    }
 }
